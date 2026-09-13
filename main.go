d7648d1dc3ce6dcb21b1c8bfb65d70432908d826
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2026-09-13 12:20:41 +0530

    feat: implement window state persistence, Windows 11 Mica material, and minimize-to-tray

--- main.go
+++ main.go
@@ -22,6 +22,41 @@ const (
 	windowWidth   = 1180
 	windowHeight  = 760
 	healthTimeout = 20 * time.Second
+
+	GWLP_WNDPROC     = -4
+	WM_CLOSE         = 0x0010
+	WM_SYSCOMMAND    = 0x0112
+	SC_MINIMIZE      = 0xF020
+	WM_USER          = 0x0400
+	WM_TRAYICON      = WM_USER + 100
+	WM_LBUTTONUP     = 0x0202
+	WM_LBUTTONDBLCLK = 0x0203
+	WM_RBUTTONUP     = 0x0205
+
+	NIM_ADD    = 0x00000000
+	NIM_MODIFY = 0x00000001
+	NIM_DELETE = 0x00000002
+
+	NIF_MESSAGE = 0x00000001
+	NIF_ICON    = 0x00000002
+	NIF_TIP     = 0x00000004
+
+	SW_HIDE          = 0
+	SW_SHOWNORMAL    = 1
+	SW_SHOWMAXIMIZED = 3
+	SW_SHOW          = 5
+	SW_RESTORE       = 9
+
+	ID_TRAY_SHOW = 1001
+	ID_TRAY_NEW  = 1002
+	ID_TRAY_QUIT = 1003
+
+	TPM_RETURNCMD   = 0x0100
+	TPM_RIGHTBUTTON = 0x0002
+	MF_SEPARATOR    = 0x0800
+	MF_STRING       = 0x0000
+
+	MONITOR_DEFAULTTONULL = 0
 )
 
 var (
@@ -40,14 +75,30 @@ var (
 	procProcess32NextW           = modKernel32.NewProc("Process32NextW")
 	procCloseHandle              = modKernel32.NewProc("CloseHandle")
 
-	modUser32            = syscall.NewLazyDLL("user32.dll")
-	procGetSystemMetrics = modUser32.NewProc("GetSystemMetrics")
-	procSetWindowPos     = modUser32.NewProc("SetWindowPos")
-	procLoadIconW        = modUser32.NewProc("LoadIconW")
-	procSendMessageW     = modUser32.NewProc("SendMessageW")
+	modUser32               = syscall.NewLazyDLL("user32.dll")
+	procGetSystemMetrics    = modUser32.NewProc("GetSystemMetrics")
+	procSetWindowPos        = modUser32.NewProc("SetWindowPos")
+	procLoadIconW           = modUser32.NewProc("LoadIconW")
+	procSendMessageW        = modUser32.NewProc("SendMessageW")
+	procGetWindowRect       = modUser32.NewProc("GetWindowRect")
+	procIsZoomed            = modUser32.NewProc("IsZoomed")
+	procShowWindow          = modUser32.NewProc("ShowWindow")
+	procSetForegroundWindow = modUser32.NewProc("SetForegroundWindow")
+	procMonitorFromRect     = modUser32.NewProc("MonitorFromRect")
+	procSetWindowLongPtrW   = modUser32.NewProc("SetWindowLongPtrW")
+	procCallWindowProcW     = modUser32.NewProc("CallWindowProcW")
+	procCreatePopupMenu     = modUser32.NewProc("CreatePopupMenu")
+	procAppendMenuW         = modUser32.NewProc("AppendMenuW")
+	procTrackPopupMenu      = modUser32.NewProc("TrackPopupMenu")
+	procDestroyMenu         = modUser32.NewProc("DestroyMenu")
+	procGetCursorPos        = modUser32.NewProc("GetCursorPos")
 
 	modShell32                                  = syscall.NewLazyDLL("shell32.dll")
+	procShell_NotifyIconW                       = modShell32.NewProc("Shell_NotifyIconW")
 	procSetCurrentProcessExplicitAppUserModelID = modShell32.NewProc("SetCurrentProcessExplicitAppUserModelID")
+
+	origWndProc uintptr
+	reallyQuit  bool
 )
 
 type PROCESSENTRY32W struct {
@@ -61,6 +112,40 @@ type PROCESSENTRY32W struct {
 	PcPriClassBase      int32
 	DwFlags             uint32
 	SzExeFile           [260]uint16
+}
+
+type RECT struct {
+	Left, Top, Right, Bottom int32
+}
+
+type POINT struct {
+	X, Y int32
+}
+
+type WindowState struct {
+	X         int  `json:"x"`
+	Y         int  `json:"y"`
+	Width     int  `json:"width"`
+	Height    int  `json:"height"`
+	Maximized bool `json:"maximized"`
+}
+
+type NOTIFYICONDATAW struct {
+	CbSize           uint32
+	HWnd             uintptr
+	UID              uint32
+	UFlags           uint32
+	UCallbackMessage uint32
+	HIcon            uintptr
+	SzTip            [128]uint16
+	DwState          uint32
+	DwStateMask      uint32
+	SzInfo           [256]uint16
+	TimeoutOrVersion uint32
+	SzInfoTitle      [64]uint16
+	DwInfoFlags      uint32
+	GuidItem         [16]byte
+	HBalloonIcon     uintptr
 }
 
 func getAppIdentity() (title string, dirName string, port int, isEnhanced bool, exeBaseName string) {
@@ -97,16 +182,76 @@ func countGatewayWindows(exeBaseName string) int {
 	return count
 }
 
-func setWindowIcon(hwnd uintptr) {
+func getAppIcon() uintptr {
 	hInst, _, _ := procGetModuleHandleW.Call(0)
 	hIcon, _, _ := procLoadIconW.Call(hInst, uintptr(1))
+	return hIcon
+}
+
+func setWindowIcon(hwnd uintptr, hIcon uintptr) {
 	if hIcon != 0 {
 		const WM_SETICON = 0x0080
-		const ICON_SMALL = 0
-		const ICON_BIG = 1
-		procSendMessageW.Call(hwnd, WM_SETICON, ICON_SMALL, hIcon)
-		procSendMessageW.Call(hwnd, WM_SETICON, ICON_BIG, hIcon)
+		procSendMessageW.Call(hwnd, WM_SETICON, 0, hIcon) // ICON_SMALL
+		procSendMessageW.Call(hwnd, WM_SETICON, 1, hIcon) // ICON_BIG
 	}
+}
+
+func getWindowStatePath(dirName string) string {
+	appData := os.Getenv("APPDATA")
+	if appData == "" {
+		home, _ := os.UserHomeDir()
+		appData = filepath.Join(home, "AppData", "Roaming")
+	}
+	return filepath.Join(appData, dirName, "window.json")
+}
+
+func saveWindowState(hwnd uintptr, dirName string) {
+	var r RECT
+	ret, _, _ := procGetWindowRect.Call(hwnd, uintptr(unsafe.Pointer(&r)))
+	if ret == 0 {
+		return
+	}
+	isZoom, _, _ := procIsZoomed.Call(hwnd)
+	ws := WindowState{
+		X:         int(r.Left),
+		Y:         int(r.Top),
+		Width:     int(r.Right - r.Left),
+		Height:    int(r.Bottom - r.Top),
+		Maximized: isZoom != 0,
+	}
+	data, err := json.MarshalIndent(ws, "", "  ")
+	if err == nil {
+		_ = os.WriteFile(getWindowStatePath(dirName), data, 0o644)
+	}
+}
+
+func restoreWindowState(hwnd uintptr, dirName string) bool {
+	data, err := os.ReadFile(getWindowStatePath(dirName))
+	if err != nil {
+		return false
+	}
+	var ws WindowState
+	if err := json.Unmarshal(data, &ws); err != nil {
+		return false
+	}
+	if ws.Width < 400 || ws.Height < 300 {
+		return false
+	}
+	r := RECT{
+		Left:   int32(ws.X),
+		Top:    int32(ws.Y),
+		Right:  int32(ws.X + ws.Width),
+		Bottom: int32(ws.Y + ws.Height),
+	}
+	hMon, _, _ := procMonitorFromRect.Call(uintptr(unsafe.Pointer(&r)), MONITOR_DEFAULTTONULL)
+	if hMon == 0 {
+		return false
+	}
+	procSetWindowPos.Call(hwnd, 0, uintptr(ws.X), uintptr(ws.Y), uintptr(ws.Width), uintptr(ws.Height), 0x0040)
+	if ws.Maximized {
+		procShowWindow.Call(hwnd, SW_SHOWMAXIMIZED)
+	}
+	return true
 }
 
 func centerWindow(hwnd uintptr, width, height int) {
@@ -154,12 +299,12 @@ func isWindowsDarkMode() bool {
 		uintptr(unsafe.Pointer(&dataSize)),
 	)
 	if ret == 0 {
-		return data == 0 // 0 means Dark mode, 1 means Light mode
+		return data == 0
 	}
 	return false
 }
 
-func applyImmersiveDarkMode(hwnd uintptr, isDark bool) {
+func applyImmersiveDarkModeAndMica(hwnd uintptr, isDark bool) {
 	if procDwmSetWindowAttribute.Find() != nil {
 		return
 	}
@@ -167,8 +312,14 @@ func applyImmersiveDarkMode(hwnd uintptr, isDark bool) {
 	if isDark {
 		val = 1
 	}
+	// DWMWA_USE_IMMERSIVE_DARK_MODE (Windows 10/11)
 	procDwmSetWindowAttribute.Call(hwnd, 20, uintptr(unsafe.Pointer(&val)), 4)
 	procDwmSetWindowAttribute.Call(hwnd, 19, uintptr(unsafe.Pointer(&val)), 4)
+
+	// DWMWA_SYSTEMBACKDROP_TYPE = 38 (Windows 11 22H2+)
+	// 2 = DWMSBT_MAINWINDOW (Mica)
+	var backdrop int32 = 2
+	procDwmSetWindowAttribute.Call(hwnd, 38, uintptr(unsafe.Pointer(&backdrop)), 4)
 }
 
 func appDataDir(dirName string) string {
@@ -198,7 +349,6 @@ func serverBinaryPath(isEnhanced bool) (string, error) {
 	if _, err := os.Stat(candidate); err == nil {
 		return candidate, nil
 	}
-	// Fallback to standard server if enhanced specific not found
 	standardCandidate := filepath.Join(dir, "DrizzleGatewayServer.exe")
 	if _, err := os.Stat(standardCandidate); err == nil {
 		return standardCandidate, nil
@@ -250,6 +400,102 @@ func stopServers(serverBinName string) {
 	killCmd := exec.Command("taskkill", "/F", "/T", "/IM", serverBinName)
 	killCmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: 0x08000000} // CREATE_NO_WINDOW
 	_ = killCmd.Run()
+}
+
+func setupTrayIcon(hwnd uintptr, appTitle string, hIcon uintptr) NOTIFYICONDATAW {
+	var nid NOTIFYICONDATAW
+	nid.CbSize = uint32(unsafe.Sizeof(nid))
+	nid.HWnd = hwnd
+	nid.UID = 1
+	nid.UFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP
+	nid.UCallbackMessage = WM_TRAYICON
+	nid.HIcon = hIcon
+
+	tipChars, _ := syscall.UTF16FromString(appTitle)
+	for i := 0; i < len(tipChars) && i < len(nid.SzTip); i++ {
+		nid.SzTip[i] = tipChars[i]
+	}
+
+	procShell_NotifyIconW.Call(NIM_ADD, uintptr(unsafe.Pointer(&nid)))
+	return nid
+}
+
+func showTrayContextMenu(hwnd uintptr, w webview.WebView, dirName string, pNid *NOTIFYICONDATAW, exePath string) {
+	hMenu, _, _ := procCreatePopupMenu.Call()
+	if hMenu == 0 {
+		return
+	}
+	defer procDestroyMenu.Call(hMenu)
+
+	titleStr, _ := syscall.UTF16PtrFromString("Show Window")
+	newWinStr, _ := syscall.UTF16PtrFromString("Open New Window")
+	quitStr, _ := syscall.UTF16PtrFromString("Quit Drizzle Gateway")
+
+	procAppendMenuW.Call(hMenu, MF_STRING, ID_TRAY_SHOW, uintptr(unsafe.Pointer(titleStr)))
+	procAppendMenuW.Call(hMenu, MF_STRING, ID_TRAY_NEW, uintptr(unsafe.Pointer(newWinStr)))
+	procAppendMenuW.Call(hMenu, MF_SEPARATOR, 0, 0)
+	procAppendMenuW.Call(hMenu, MF_STRING, ID_TRAY_QUIT, uintptr(unsafe.Pointer(quitStr)))
+
+	var pt POINT
+	procGetCursorPos.Call(uintptr(unsafe.Pointer(&pt)))
+	procSetForegroundWindow.Call(hwnd)
+
+	cmd, _, _ := procTrackPopupMenu.Call(
+		hMenu,
+		TPM_RETURNCMD|TPM_RIGHTBUTTON,
+		uintptr(pt.X),
+		uintptr(pt.Y),
+		0,
+		hwnd,
+		0,
+	)
+
+	switch cmd {
+	case ID_TRAY_SHOW:
+		procShowWindow.Call(hwnd, SW_SHOW)
+		procShowWindow.Call(hwnd, SW_RESTORE)
+		procSetForegroundWindow.Call(hwnd)
+	case ID_TRAY_NEW:
+		_ = exec.Command(exePath).Start()
+	case ID_TRAY_QUIT:
+		procShell_NotifyIconW.Call(NIM_DELETE, uintptr(unsafe.Pointer(pNid)))
+		saveWindowState(hwnd, dirName)
+		reallyQuit = true
+		w.Terminate()
+	}
+}
+
+func initSubclass(hwnd uintptr, w webview.WebView, dirName string, pNid *NOTIFYICONDATAW, exePath string) {
+	callback := syscall.NewCallback(func(h uintptr, msg uint32, wParam uintptr, lParam uintptr) uintptr {
+		switch msg {
+		case WM_SYSCOMMAND:
+			if (wParam & 0xFFF0) == SC_MINIMIZE {
+				saveWindowState(h, dirName)
+				procShowWindow.Call(h, SW_HIDE)
+				return 0 // Minimize to tray!
+			}
+		case WM_CLOSE:
+			saveWindowState(h, dirName)
+			procShell_NotifyIconW.Call(NIM_DELETE, uintptr(unsafe.Pointer(pNid)))
+			reallyQuit = true
+		case WM_TRAYICON:
+			switch lParam {
+			case WM_LBUTTONUP, WM_LBUTTONDBLCLK:
+				procShowWindow.Call(h, SW_SHOW)
+				procShowWindow.Call(h, SW_RESTORE)
+				procSetForegroundWindow.Call(h)
+				return 0
+			case WM_RBUTTONUP:
+				showTrayContextMenu(h, w, dirName, pNid, exePath)
+				return 0
+			}
+		}
+		ret, _, _ := procCallWindowProcW.Call(origWndProc, h, uintptr(msg), wParam, lParam)
+		return ret
+	})
+
+	orig, _, _ := procSetWindowLongPtrW.Call(hwnd, ^uintptr(3), callback)
+	origWndProc = orig
 }
 
 func getLoadingHTML(isDark bool, message string) string {
@@ -331,9 +577,18 @@ func main() {
 	w.SetSize(windowWidth, windowHeight, webview.HintNone)
 
 	hwnd := uintptr(w.Window())
-	setWindowIcon(hwnd)
-	applyImmersiveDarkMode(hwnd, isDark)
-	centerWindow(hwnd, windowWidth, windowHeight)
+	hIcon := getAppIcon()
+	setWindowIcon(hwnd, hIcon)
+	applyImmersiveDarkModeAndMica(hwnd, isDark)
+
+	// Restore last saved window geometry, or center if first run
+	if !restoreWindowState(hwnd, dirName) {
+		centerWindow(hwnd, windowWidth, windowHeight)
+	}
+
+	exePath, _ := os.Executable()
+	nid := setupTrayIcon(hwnd, appTitle, hIcon)
+	initSubclass(hwnd, w, dirName, &nid, exePath)
 
 	serverBin, err := serverBinaryPath(isEnhanced)
 	serverBinName := "DrizzleGatewayServer.exe"
@@ -355,6 +610,9 @@ func main() {
 			return
 		}
 		cleaned = true
+
+		procShell_NotifyIconW.Call(NIM_DELETE, uintptr(unsafe.Pointer(&nid)))
+		saveWindowState(hwnd, dirName)
 
 		if countGatewayWindows(exeBase) <= 1 {
 			cmdMu.Lock()
@@ -427,3 +685,4 @@ func main() {
 	w.Run()
 	cleanupOnce()
 }
+
